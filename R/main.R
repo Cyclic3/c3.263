9efc435e663d4309003c4e86a3af07cb26dc06a8
Cyclic3 <cyclic3.git@gmail.com>
2021-05-17 14:12:37 +0100

    Some documentation improvements

--- R/main.R
+++ R/main.R
@@ -1,9 +1,18 @@
+#' Opens a directory selection dialog box
+#'
+#' @return The selected directory, or NULL if cancelled
 #' @export
 prompt_dir <- tcltk::tk_choose.dir
 
+#' Opens a file selection dialog box
+#' 
+#' @return The selected file, or NULL if cancelled
 #' @export
 prompt_file <- tcltk::tk_choose.files
 
+#' Opens a file selection dialog box for common extensions
+#'
+#' @return The selected file, or NULL if cancelled
 #' @export
 prompt_datafile <- function() {
   prompt_file(caption="Select data file", multi=F, filters = matrix(c(
@@ -16,6 +25,9 @@ prompt_datafile <- function() {
   ), 6, 2, byrow=T))
 }
 
+#' Reads data directly copied from Mobius
+#'
+#' @return The loaded data as a dataframe
 #' @export
 prompt_mobius <- function() {
   print("Paste the data from the Mobius table, followed by an empty line:")
@@ -29,16 +41,22 @@ prompt_mobius <- function() {
   }
   str = paste(lines, sep="\n")
 
-  read.csv(text=str, sep="\t")
+  utils::read.csv(text=str, sep="\t")
 }
 
+#' Load an arbitrary input file
+#'
+#' This function automatically selects the format based on the extension
+#'
+#' @param filename Path to the input file
+#' @return Data parsed from filename
 #' @export
 read.data <- function (filename) {
   switch (tools::file_ext(filename),
-    "csv" = read.csv(filename),
+    "csv" = utils::read.csv(filename),
     "xls" = ,
     "xlsx" = readxl::read_excel(filename),
     "rds" = ,
-    "Rds" = readRDS(filename)
+    "Rds" = base::readRDS(filename)
   )
 }
